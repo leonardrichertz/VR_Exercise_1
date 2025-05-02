237b03d1d3ae7fa26d81d669631f026b614f3b90
sam1raa <sjost111@gmx.de>
2025-05-02 23:39:21 +0200

    Finished Teleport Doors

---
+++ Assets/Scripts/SpawnPosition.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpawnPosition : MonoBehaviour
+{
+    [SerializeField] Transform[] spawnPositions;
+    [SerializeField] Transform player;
+    [SerializeField] Transform camera;
+
+    void Start()
+    {
+        PickRandomPosition();
+    }
+
+    void PickRandomPosition()
+    {
+        Transform spawnPosition = spawnPositions[Random.Range(0, 2)];
+        player.position = spawnPosition.position;
+        camera.position = spawnPosition.position;
+        player.rotation = spawnPosition.rotation;
+        camera.rotation = spawnPosition.rotation;
+    }
+}
---
+++ Assets/Scripts/TeleportDoorInterior.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TeleportDoorInterior : MonoBehaviour
+{
+    [SerializeField] int doorIndex;
+    
+    void OnTriggerEnter(Collider col)
+    {
+        if(col.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
