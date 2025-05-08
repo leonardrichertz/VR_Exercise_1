cf500b1fe449e80f87fa0560cb1b638e5a40e7a5
sam1raa <sjost111@gmx.de>
2025-05-08 21:21:05 +0200

    working apk

--- Assets/Scripts/TeleportDoor.cs
+++ Assets/Scripts/TeleportDoor.cs
@@ -4,20 +4,12 @@ using UnityEngine.SceneManagement;
 public class TeleportDoor : MonoBehaviour
 {
     [SerializeField] int doorIndex;
-    [SerializeField] bool vr;
     
     void OnTriggerEnter(Collider col)
     {
         if(col.tag == "Player")
         {
-            if(vr)
-            {
-                SceneManager.LoadScene(3);
-            }
-            else
-            {
-                SceneManager.LoadScene(1);
-            }
+            SceneManager.LoadScene(0);
         }
     }
 }
--- Assets/Scripts/TeleportDoorInterior.cs
+++ Assets/Scripts/TeleportDoorInterior.cs
@@ -4,20 +4,12 @@ using UnityEngine.SceneManagement;
 public class TeleportDoorInterior : MonoBehaviour
 {
     [SerializeField] int doorIndex;
-    [SerializeField] bool vr;
     
     void OnTriggerEnter(Collider col)
     {
         if(col.tag == "Player")
         {
-            if(vr)
-            {
-                SceneManager.LoadScene(2);
-            }
-            else
-            {
-                SceneManager.LoadScene(0);
-            }
+            SceneManager.LoadScene(1);
         }
     }
 }
